7752322e80969a2cf4dbb57241edcabaaa8728ba
christianekenstedt <christian>
2016-10-17 16:40:55 +0200

    Add project files.

---
+++ Lab2Community/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab2Community.MvcApplication" Language="C#" %>
---
+++ Lab2Community/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab2Community.Startup))]
+namespace Lab2Community
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
