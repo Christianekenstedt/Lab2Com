1a2a3b3957fe9a757e28baa6b6922771f2c8779f
antonwass <anton_wass@hotmail.com>
2016-10-19 18:39:23 +0200

    view/add groups

---
+++ Lab2Community/Models/View/UserGroupViewModels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab2Community.Models.View
+{
+    public class ShortViewUserGroupViewModel
+    {
+
+        public int GroupId { get; set; }
+
+        public string Name { get; set; }
+
+
+    }
+
+    public class CreateUserGroupViewModel
+    {
+        public string Name { get; set; }
+    }
+
+    public class UserGroupDetailsViewModel
+    {
+        public int GroupId { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<ApplicationUser> Members { get; set; }
+
+    }
+
+}
